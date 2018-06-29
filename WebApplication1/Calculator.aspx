6c983f8246944930f654154a1b03d34e7f83d8a3
Kacper <30697294+Chudy20007@users.noreply.github.com>
2018-06-29 17:59:31 +0200

    First commit

---
+++ WebApplication1/Calculator.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="WebApplication1.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+ <title>Calculator</title>
+    	
+
+
+<link href="~/main.css" rel="stylesheet" type="text/css" media="screen" runat="server" />
+</head>
+<body>
+    <form id="form1" method="post" runat="server">
+        <div id="container">
+
+
+            <div id="calculator">
+
+
+                &nbsp;&nbsp;
+                <br />
+                <div id="TextFields">
+                    &nbsp;&nbsp;
+                    <asp:Label id="tbValues" runat="server" class="fields" style="text-align:right; Width:100px;height:40px"></asp:Label>
+                    <asp:Label id="tbResult" runat="server" class="fields" style="text-align:left; Width:100px;height:40px"></asp:Label>
+                </div>
+
+                <div id="buttonsPanel">
+
+                    <div id="addPanel">
+
+                         <asp:Button id="ButtonPower" class="buttons" type="button" runat="server" onclick="ButtonPower_Click" Text="^2" />  <asp:Button id="ButtonPercent" class="buttons" type="button" runat="server" onclick="ButtonPercent_Click" Text="%" />  <asp:Button id="ButtonRad" class="buttons" type="button" runat="server" onclick="ButtonRad_Click" Text="(sqrt)" /><br />
+                         <asp:Button id="ButtonMul" class="buttons" type="button" runat="server" onclick="ButtonMul_Click" Text="*" /> <asp:Button id="ButtonDiv" class="buttons" type="button" runat="server" onclick="ButtonDiv_Click" Text="/" />  <asp:Button id="ButtonClear" class="buttons" type="button" runat="server" onclick="ButtonClear_Click" Text="C" /><br />
+                         <asp:Button id="ButtonAdd" class="buttons" type="button" runat="server" onclick="ButtonAdd_Click" Text="+" /> <asp:Button id="ButtonSubb" class="buttons" type="button" runat="server" onclick="ButtonSubb_Click" Text="-" /><br />
+                         <asp:Button id="ButtonResult" style="width:103px" class="buttons" type="button" runat="server" onclick="ButtonResult_Click" Text="=" />
+
+                    </div>
+                    <br />
+
+                    <asp:Button id="Button1" class="buttons" type="button" runat="server" onclick="Button1_Click" Text="1" /> <asp:Button id="Button2" class="buttons" type="button" runat="server" onclick="Button2_Click" Text="2" /><asp:Button id="Button3" class="buttons" type="button" runat="server" onclick="Button3_Click" Text="3" />
+                    <br />
+                    <asp:Button id="Button4" class="buttons" type="button" runat="server"  onclick="Button4_Click" Text="4" /> <asp:Button id="Button5" class="buttons" type="button" runat="server" onclick="Button5_Click" Text="5" /><asp:Button id="Button6" class="buttons" type="button" runat="server" onclick="Button6_Click" Text="6" /> <br />
+                    <asp:Button id="Button7" class="buttons" type="button" runat="server" onclick="Button7_Click" Text="7" /> <asp:Button id="Button8" class="buttons" type="button" runat="server" onclick="Button8_Click" Text="8" /><asp:Button id="Button9" class="buttons" type="button" runat="server" onclick="Button9_Click" Text="9" /><br />
+                    <asp:Button id="Button0" style="width:100px" class="buttons" type="button" runat="server" onclick="Button0_Click" Text="0" /> <asp:Button id="ButtonD" class="buttons" type="button" runat="server" onclick="ButtonD_Click" Text="," />
+                </div>
+
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Calculator.aspx.cs
@@ -0,0 +1,496 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "0";
+            }
+            else
+            tbValues.Text += "0";
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "1";
+            }
+            else
+                tbValues.Text += "1";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "2";
+            }
+            else
+                tbValues.Text += "2";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "3";
+            }
+            else
+                tbValues.Text += "3";
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "4";
+            }
+            else
+                tbValues.Text += "4";
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "5";
+            }
+            else
+                tbValues.Text += "5";
+        }
+
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "6";
+            }
+            else
+                tbValues.Text += "6";
+        }
+
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "7";
+            }
+            else
+                tbValues.Text += "7";
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "8";
+            }
+            else
+                tbValues.Text += "8";
+        }
+
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (emptyValues() == false)
+            {
+                tbResult.Text = "";
+                tbValues.Text = "9";
+            }
+            else
+                tbValues.Text += "9";
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() ==true)
+            {
+                return;
+            }
+            if (tbResult.Text.Length > 0)
+            {
+                tbValues.Text = "";
+                int count = 1;
+                do
+                {
+                    tbValues.Text += tbResult.Text[count];
+                    count++;
+                } while (count < tbResult.Text.Length);
+
+                tbResult.Text = "";
+            }
+            tbValues.Text += "+";
+        }
+
+        protected void ButtonSubb_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() == true)
+            {
+                return;
+            }
+            if (tbResult.Text.Length > 0)
+            {
+               
+                tbValues.Text = "";
+                int count = 1;
+                do
+                {
+                    tbValues.Text += tbResult.Text[count];
+                    count++;
+                } while (count < tbResult.Text.Length);
+
+                tbResult.Text = "";
+            }
+            tbValues.Text += "-";
+        }
+
+        protected void ButtonPercent_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() == true)
+            {
+                return;
+            }
+
+            if (tbResult.Text.Length > 0)
+            {
+                tbValues.Text = "";
+                int count = 1;
+                do
+                {
+                    tbValues.Text += tbResult.Text[count];
+                    count++;
+                } while (count < tbResult.Text.Length);
+
+                tbResult.Text = "";
+            }
+            tbValues.Text += "%";
+        }
+
+        protected void ButtonDiv_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() == true)
+            {
+                return;
+            }
+
+            if (tbResult.Text.Length > 0)
+            {
+                tbValues.Text = "";
+                int count = 1;
+                do
+                {
+                    tbValues.Text += tbResult.Text[count];
+                    count++;
+                } while (count < tbResult.Text.Length);
+
+                tbResult.Text = "";
+            }
+            tbValues.Text += "/";
+        }
+
+        protected void ButtonD_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() == true)
+            {
+                return;
+            }
+
+            if (tbValues.Text.Length == 0)
+            {
+                tbValues.Text += "0,";
+                return;
+            }
+            int count = 0;
+           bool isComma = false;
+            while (count < tbValues.Text.Length && (tbValues.Text[count] != '+' || tbValues.Text[count] != '-'
+                || tbValues.Text[count] != '*' || tbValues.Text[count] != '/' || tbValues.Text[count] != '%'))
+            {
+
+               
+               if (tbValues.Text[count] == ',')
+                {
+                    isComma = true;
+                }
+                
+
+                if (isComma == false && count == tbValues.Text.Length - 1)
+                {
+
+                    tbValues.Text += ",";
+                    return;
+                }
+                count++;
+            }
+               if ((tbValues.Text[tbValues.Text.Length-1] == '+' || tbValues.Text[tbValues.Text.Length-1] == '-' 
+                    || tbValues.Text[tbValues.Text.Length-1] == '*' || tbValues.Text[tbValues.Text.Length-1] == '/' ||
+                    tbValues.Text[tbValues.Text.Length-1] == '%') && (tbValues.Text[tbValues.Text.Length - 1] != ',' || tbValues.Text[tbValues.Text.Length - 2] != ','))
+                {
+                    tbValues.Text += "0,";
+                return;
+                }
+
+               
+                if (tbValues.Text.Length>0 && tbValues.Text[tbValues.Text.Length - 1] != ',')
+                {
+                tbValues.Text += ",";
+                return;
+                }
+                
+        }
+
+        protected void ButtonResult_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string value1=null, value2=null;
+            string operationChar=null;
+            bool findChar = false;
+
+            if (tbValues.Text.Length == 0)
+            {
+                tbValues.Text = "0";
+                return;
+            }
+
+            else
+                try
+                {
+                    do
+                    {
+
+
+                        if (tbValues.Text[count] == ('+'))
+                        {
+                            operationChar = "+";
+                            findChar = true;
+                            break;
+                        }
+
+                        if (tbValues.Text[count] == ('-'))
+                        {
+                            operationChar = "-";
+                            findChar = true;
+                            break;
+                        }
+
+                        if (tbValues.Text[count] == ('*'))
+                        {
+                            operationChar = "*";
+                            findChar = true;
+                            break;
+                        }
+
+                        if (tbValues.Text[count] == ('/'))
+                        {
+                            operationChar = "/";
+                            findChar = true;
+                            break;
+                        }
+
+                        if (tbValues.Text[count] == ('%'))
+                        {
+                            operationChar = "%";
+                            findChar = true;
+                            break;
+
+                        }
+                        value1 += tbValues.Text[count];
+                        count++;
+
+                    } while ((findChar != true));
+
+                }
+                catch
+                {
+                    tbValues.Text = "ERROR";
+                }
+            count++;
+            try
+            {
+                do
+                {
+                    value2 += tbValues.Text[count];
+                    count++;
+                } while (count < tbValues.Text.Length);
+            }
+            catch
+            {
+                tbValues.Text += "";
+                tbResult.Text += "";
+            }
+
+            if ((Calculate(value1, value2, operationChar)).ToString() == "0")
+            {
+                tbValues.Text = "";
+                tbResult.Text = ("Niedozwolona operacja!");
+                return;
+            }
+            else
+            {
+                tbResult.Text = "=" + (Calculate(value1, value2, operationChar)).ToString();
+            }
+
+        }
+
+        protected double Calculate (string val1, string val2, string Char)
+        {
+            double result=0;
+            try
+            {
+                if (Char == "+")
+                    return result = double.Parse(val1) + double.Parse(val2);
+
+                if (Char == "-")
+                    return result = double.Parse(val1) - double.Parse(val2);
+
+                if (Char == "*")
+                    return result = double.Parse(val1) * double.Parse(val2);
+
+                if (Char == "/" && val2 != "0")
+                    return result = double.Parse(val1) / double.Parse(val2);
+
+                if (Char == "%")
+                    return result = double.Parse(val1) % double.Parse(val2);
+            }
+            catch
+            {
+                tbValues.Text = "ERROR";
+            }
+
+            return 0;
+            
+
+           
+            
+            
+        }
+
+        protected void ButtonRad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbResult.Text.Length == 0)
+                    tbResult.Text = "=" + Math.Sqrt(double.Parse(tbValues.Text)).ToString();
+                else
+                {
+                    char[] OperationsChars = { '=', '+', '-', '*', '/', '%' };
+
+                    string result = tbResult.Text.TrimStart(OperationsChars);
+                    tbValues.Text = tbResult.Text.TrimStart(OperationsChars);
+                    tbResult.Text = "=" + Math.Sqrt(double.Parse(result)).ToString();
+                }
+            }
+            catch
+            {
+                tbResult.Text = "";
+                tbValues.Text = "";
+            }
+
+        }
+
+        protected bool checkValueChar ()
+        {
+            try
+            {
+                if (tbValues.Text[tbValues.Text.Length - 1] == '+' || tbValues.Text[tbValues.Text.Length - 1] == '*' || tbValues.Text[tbValues.Text.Length - 1] == '/'
+                    || tbValues.Text[tbValues.Text.Length - 1] == '-' || tbValues.Text[tbValues.Text.Length - 1] == '%')
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        protected void ButtonPower_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (emptyValues() == false)
+                {
+
+                    tbValues.Text = Math.Pow(double.Parse(tbResult.Text), 2).ToString();
+                    tbResult.Text = "";
+                }
+                else
+                {
+                    double x = double.Parse(tbValues.Text);
+                    tbValues.Text = "";
+                    tbResult.Text = Math.Pow(x, 2).ToString();
+                }
+            }
+            catch
+            {
+                tbValues.Text = "";
+                tbResult.Text = "";
+            }
+
+        }
+
+       
+
+        protected void ButtonMul_Click(object sender, EventArgs e)
+        {
+            if (checkValueChar() == true)
+            {
+                return;
+            }
+
+            if (tbResult.Text.Length > 0)
+            {
+                tbValues.Text = "";
+                int count = 1;
+                do
+                {
+                    tbValues.Text += tbResult.Text[count];
+                    count++;
+                } while (count < tbResult.Text.Length);
+
+                tbResult.Text = "";
+            }
+            tbValues.Text += "*";
+        }
+        protected bool emptyValues ()
+        {
+            if (tbResult.Text.Length > 0)
+                return false;
+
+            return true;
+        }
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            tbValues.Text = "";
+            tbResult.Text = "";
+        }
+    }
+}
